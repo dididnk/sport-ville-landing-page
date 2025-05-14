7c5eb6b259b19e7f7e6ae5656bdd5780aa84d821
Emmanuel NGBAME <ngbamedistingue@yahoo.com>
2025-05-14 16:55:39 +0200

    feat: redirreection fix ex 1

--- sportville-landing-page/vite.config.ts
+++ sportville-landing-page/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        404: './404.html'
+      }
+    }
+  }
 });
